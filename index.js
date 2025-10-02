44b307b24e14cddf7a6f9b5243b0ecd2619933bd
yasntrk_ <yturk@eseye.com>
2025-10-02 18:59:48 +0300

    Add error handling middleware for improved error responses

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ dotenv.config();
 app.use(express.json());
 connectionDB()
 app.use('/api', routes);
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
